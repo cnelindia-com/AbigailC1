aba5c9c4ffe94fe6025e891ba586283cc1a7dc3d
Siyaram Malav <rahula567@gmail.com>
2022-02-09 17:00:42 +0530

    Update edit_page.php

--- src/public/edit_page.php
+++ src/public/edit_page.php
@@ -169,41 +169,7 @@ if(isset($_GET['shop'])&&isset($_GET['page-id'])){
 			z-index: 20;
 		}
 		
-			
-			.post{
-				width: 97%;
-				min-height: 200px;
-				padding: 5px;
-				border: 1px solid gray;
-				margin-bottom: 15px;
-			}
 
-			.post h1{
-				letter-spacing: 1px;
-				font-weight: normal;
-				font-family: sans-serif;
-			}
-
-
-			.post p{
-				letter-spacing: 1px;
-				text-overflow: ellipsis;
-				line-height: 25px;
-			}
-
-			/* Load more */
-			.load-more{
-				width: 99%;
-				background: #15a9ce;
-				text-align: center;
-				color: white;
-				padding: 10px 0px;
-				font-family: sans-serif;
-			}
-
-			.load-more:hover{
-				cursor: pointer;
-			}
 
 			
 		</style>
